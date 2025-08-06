01da130974d5c378fddd5eddb50eec0dc4bcf167
lenienguyen <lenie.nguy10@gmail.com>
2025-08-06 23:53:40 +0200

    Build homepage

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ const josefin = Josefin_Sans({
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -23,16 +24,13 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
 
-        <main>{children}</main>
-
-        <footer>Copyright by The Wild Oasis</footer>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
