ef621db897493cc42f7b6ea1c91b8fa21c5bb685
lenienguyen <lenie.nguy10@gmail.com>
2025-08-09 23:03:50 +0200

    feat(cabins): filter by size

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -41,10 +41,10 @@ const Page = () => {
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <Image
+            <img
               src={countryFlag}
               alt="Country flag"
-              className="h-5 rounded-sm"
+              className="h-5 rounded-sm object-cover"
             />
           </div>
 
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,17 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
 };
 
-const Page = async () => {
+const Page = async ({ searchParams }) => {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +25,13 @@ const Page = async () => {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
