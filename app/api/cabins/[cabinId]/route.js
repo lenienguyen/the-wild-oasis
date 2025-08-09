0011dbb3f49f26b4f236363679458a9ecd972852
lenienguyen <lenie.nguy10@gmail.com>
2025-08-10 00:17:49 +0200

    Custom endpoint with route handlers

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,17 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export const GET = async (request, { params }) => {
+  const { cabinId } = params;
+  console.log("hi");
+  console.log(cabinId);
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found" });
+  }
+};
