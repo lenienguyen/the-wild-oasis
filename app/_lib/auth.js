7c009ddd4f50d8880ddfa87811fbfd4531ae3506
lenienguyen <lenie.nguy10@gmail.com>
2025-08-10 12:15:25 +0200

    feat(auth): create new guest on first sign in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const {
   auth,
@@ -11,6 +12,29 @@ export const {
   callbacks: {
     authorized: async ({ auth, request }) => {
       return !!auth;
+    },
+    signIn: async ({ user, account, profile }) => {
+      try {
+        // getGuest returns null if user does not exist
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    session: async ({ session, user }) => {
+      const guest = await getGuest(session.user.email);
+
+      // update user session guestId with supabase id
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
