ce704569b915a9a9fec649a9c3e675902cc54e82
lenienguyen <lenie.nguy10@gmail.com>
2025-08-10 14:11:32 +0200

    feat(user): form update profile display loading indicator

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 const UpdateProfileForm = ({ children, guest }) => {
   const [count, setCount] = useState();
@@ -56,9 +58,7 @@ const UpdateProfileForm = ({ children, guest }) => {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <SubmitButton pendingLabel="Upading...">Update profile</SubmitButton>
       </div>
     </form>
   );
